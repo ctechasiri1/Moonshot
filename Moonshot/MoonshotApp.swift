b3175f5051eb2df4af5971d9d7f3defe26bfee6b
Chiraphat <ctechasiri@gmail.com>
2024-10-30 20:37:02 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Chiraphat Techasiri on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
