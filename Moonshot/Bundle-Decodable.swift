d2b5f3ed81c588ad26ba3ef0ea4c55400000bd31
Chiraphat <ctechasiri@gmail.com>
2024-11-03 00:58:13 -0700

    [Feature] Added the home screen to the app

--- Moonshot/Bundle-Decodable.swift
+++ Moonshot/Bundle-Decodable.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Bundle {
-    func decode<T: Codable>(_ file: String) -> T {
+    func decode<T: Decodable>(_ file: String) -> T {
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
@@ -17,6 +17,9 @@ extension Bundle {
         }
         
         let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
         
         do {
             return try decoder.decode(T.self, from: data)
